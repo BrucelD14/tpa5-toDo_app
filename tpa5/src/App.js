0d465d50d7b027d4d1795e7a2c01b589ad4dee1c
BrucelD14 <samuderaduta144@gmail.com>
2022-11-14 19:31:20 +0700

    add AppHeader & button

--- tpa5/src/App.js
+++ tpa5/src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
-import PageTitle from './components/PageTitle';
+import "./App.css";
+import AppHeader from "./components/AppHeader";
+import PageTitle from "./components/PageTitle";
+import style from "./styles/modules/app.module.scss";
 
 function App() {
   return (
-    <div className="App">
-    <PageTitle>TODO LIST</PageTitle>
+    <div className="container">
+      <PageTitle>TODO LIST</PageTitle>
+      <div className={style.app__wrapper}>
+      <AppHeader></AppHeader>
+      </div>
     </div>
   );
 }
---
+++ tpa5/src/components/AppHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AppHeader() {
+  return (
+    <div>
+    <h1>HELLO FROM HEADER</h1>
+    </div>
+  )
+}
+
+export default AppHeader
