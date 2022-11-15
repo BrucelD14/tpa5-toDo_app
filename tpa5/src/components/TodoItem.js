9708897b26198264bc4d01ee054c42af4174203f
BrucelD14 <samuderaduta144@gmail.com>
2022-11-15 18:28:46 +0700

    on working 6

--- tpa5/src/components/TodoItem.js
+++ tpa5/src/components/TodoItem.js
@@ -3,8 +3,22 @@ import styles from "../styles/modules/todoItem.module.scss";
 import { getClasses } from "../utils/getClasses";
 import { format } from "date-fns/esm";
 import { MdDelete, MdEdit } from "react-icons/md";
+import { useDispatch } from "react-redux";
+import { deleteTodo } from "../slices/todoSlice";
+import toast from "react-hot-toast";
 
 function TodoItem({ todo }) {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    dispatch(deleteTodo(todo.id));
+    toast.success("Todo Deleted Successfully");
+  };
+
+  const handleUpdate = () => {
+    console.log("updating");
+  };
+
   return (
     <div className={styles.item}>
       <div className={styles.todoDetails}>
@@ -24,10 +38,22 @@ function TodoItem({ todo }) {
         </div>
       </div>
       <div className={styles.todoActions}>
-        <div className={styles.icon}>
+        <div
+          className={styles.icon}
+          onClick={handleDelete}
+          onKeyDown={handleDelete}
+          role="button"
+          tabIndex={0}
+        >
           <MdDelete />
         </div>
-        <div className={styles.icon}>
+        <div
+          className={styles.icon}
+          onClick={handleUpdate}
+          onKeyDown={handleUpdate}
+          role="button"
+          tabIndex={0}
+        >
           <MdEdit />
         </div>
       </div>
