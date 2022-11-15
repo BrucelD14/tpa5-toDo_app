ac2c5777ef0ec4cf2cdeb955459a731fa339a33a
BrucelD14 <samuderaduta144@gmail.com>
2022-11-15 15:57:40 +0700

    on working 5

--- tpa5/src/components/TodoItem.js
+++ tpa5/src/components/TodoItem.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "../styles/modules/todoItem.module.scss";
 import { getClasses } from "../utils/getClasses";
+import { format } from "date-fns/esm";
+import { MdDelete, MdEdit } from "react-icons/md";
 
 function TodoItem({ todo }) {
   return (
@@ -16,6 +18,17 @@ function TodoItem({ todo }) {
           >
             {todo.title}
           </p>
+          <p className={styles.time}>
+            {format(new Date(todo.time), "p, MM/dd/yyyy")}
+          </p>
+        </div>
+      </div>
+      <div className={styles.todoActions}>
+        <div className={styles.icon}>
+          <MdDelete />
+        </div>
+        <div className={styles.icon}>
+          <MdEdit />
         </div>
       </div>
     </div>
