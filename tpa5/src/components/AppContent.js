3e9b3e887eec3c74c73e739a1faa0a578a08d14c
BrucelD14 <samuderaduta144@gmail.com>
2022-11-16 11:59:34 +0700

    on working 11

--- tpa5/src/components/AppContent.js
+++ tpa5/src/components/AppContent.js
@@ -2,6 +2,26 @@ import React from "react";
 import { useSelector } from "react-redux";
 import TodoItem from "./TodoItem";
 import styles from "../styles/modules/app.module.scss";
+import { motion } from "framer-motion";
+
+const container = {
+  hidden: { opacity: 1 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const child = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
 
 function AppContent() {
   const todoList = useSelector((state) => state.todo.todoList);
@@ -17,11 +37,20 @@ function AppContent() {
   });
 
   return (
-    <div className={styles.content__wrapper}>
-      {filteredTodoList && filteredTodoList.length > 0
-        ? filteredTodoList.map((todo) => <TodoItem key={todo.id} todo={todo} />)
-        : "no todo found"}
-    </div>
+    <motion.div
+      className={styles.content__wrapper}
+      variants={container}
+      initial="hidden"
+      animate="visible"
+    >
+      {filteredTodoList && filteredTodoList.length > 0 ? (
+        filteredTodoList.map((todo) => <TodoItem key={todo.id} todo={todo} />)
+      ) : (
+        <motion.p className={styles.emptyText} variants={child}>
+          No Todo Found
+        </motion.p>
+      )}
+    </motion.div>
   );
 }
 
